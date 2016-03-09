5dad270a4749a9387500a39a460bc18e1117a89a
Udai Singh <udaisingh@Udais-MacBook-Pro.local>
2016-03-09 01:22:59 -0500

    new UI

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,50 @@
 'use strict';
 
-angular.module('StockAnalyzerApp', ['ui.bootstrap'])
+var StockAnalyzerApp = angular.module('StockAnalyzerApp', ['ui.bootstrap']);
 
-  .controller('MenuController', ['$scope', '$http', function($scope, $http){
+  StockAnalyzerApp.controller('MenuController', function($scope, $http){
 
-    $http.get('json/tickers.json').then(function(response){
-      $scope.ticker_names = [];
-      console.log(response.data);
-      // angular.forEach(data, function(value, key){
-      //   $scope.ticker_names.push(value.tickr_name);
-      // });
-    });
+  // To demonstrate the log service
+  //  log.log("Hello");
+  //  log.info
+  //  log.warn
+  //  log.debug
+  //  log.error
 
     $scope.message = "Hello World!";
-  }]);
+    $scope.selected = undefined;
+    $scope.ticker_names = [];
+
+    var ticker_name;
+    var ticker_symbol;
+
+    $http.get('app/json/tickers.json').then(function(response){
+
+      angular.forEach(response.data.records, function(value, key){
+        $scope.ticker_names.push(value.tickr_name);
+      });
+    });
+
+    $scope.submit = function(){
+      if($scope.selected === '' || $scope.selected === undefined){
+          alert("please enter value..");
+          return;
+      };
+
+      var ticker = $scope.selected.split(',', 2);
+      ticker_name = ticker[0];
+      ticker_symbol = ticker[1];
+
+      $scope.selected = '';
+
+      var url = "http://query.yahooapis.com/v1/public/yql?q=";
+      var uriComponent = "select * from yahoo.finance.quotes where symbol in ('" + tickr_symbol + "')";
+      var data = encodeURIComponent(uriComponent);
+      var end = "&format=json&diagnostics=true&env=http://datatables.org/alltables.env";
+      var jsoncallback = "&callback=JSON_CALLBACK";
+
+      var final_url = url + data + end ;
+
+    };
+
+  });
