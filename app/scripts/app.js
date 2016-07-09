bcad111ecfce8993f9b207d627bc96d51f63abe7
Udai Singh <udaisingh@Udais-MacBook-Pro.local>
2016-07-09 02:02:18 -0400

    restart

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,8 +15,13 @@ var StockAnalyzerApp = angular.module('StockAnalyzerApp', ['ui.bootstrap']);
     $scope.selected = undefined;
     $scope.ticker_names = [];
 
+    $scope.result = [];
+
+    var ticker;
     var ticker_name;
     var ticker_symbol;
+
+    var last_trade_price;
 
     $http.get('app/json/tickers.json').then(function(response){
 
@@ -31,20 +36,32 @@ var StockAnalyzerApp = angular.module('StockAnalyzerApp', ['ui.bootstrap']);
           return;
       };
 
-      var ticker = $scope.selected.split(',', 2);
+      ticker = $scope.selected.split(',', 2);
       ticker_name = ticker[0];
       ticker_symbol = ticker[1];
 
       $scope.selected = '';
 
       var url = "http://query.yahooapis.com/v1/public/yql?q=";
-      var uriComponent = "select * from yahoo.finance.quotes where symbol in ('" + tickr_symbol + "')";
+      var uriComponent = "select * from yahoo.finance.quotes where symbol in ('" + ticker_symbol + "')";
       var data = encodeURIComponent(uriComponent);
       var end = "&format=json&diagnostics=true&env=http://datatables.org/alltables.env";
       var jsoncallback = "&callback=JSON_CALLBACK";
 
       var final_url = url + data + end ;
 
+      $http.get(final_url).then(function(response){
+      //  console.log(response);
+        last_trade_price = response.data.query.results.quote.LastTradePriceOnly;
+        $scope.result.unshift({
+          "Ticker" : ticker_name,
+          "Last_Trade_Price" : last_trade_price
+        });
+        console.log($scope.result);
+      }, function(error){
+        alert("Error occurred");
+        console.log(error);
+      });
     };
 
   });
